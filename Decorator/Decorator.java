d0b05354e73fdb4e10bfa580f7c8fde4a752be37
Tridib Banik <tridib.perfect@gmail.com>
2025-04-12 00:29:58 -0400

    Added Decorator Abstract class

---
+++ Decorator/Decorator.java
@@ -0,0 +1,12 @@
+public abstract class Decorator implements Component {
+    protected Component component;
+
+    public Decorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public void operation() {
+        component.operation();
+    }
+}
