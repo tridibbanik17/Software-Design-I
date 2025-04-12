86224b7865b0cca0d3e8231c761b887535a7d5be
Tridib Banik <tridib.perfect@gmail.com>
2025-04-12 00:29:34 -0400

    Added ConcreteDecorator class

---
+++ Decorator/ConcreteDecorator.java
@@ -0,0 +1,11 @@
+public class ConcreteDecorator extends Decorator {
+    public ConcreteDecorator(Component component) {
+        super(component);
+    }
+
+    @Override
+    public void operation() {
+        super.operation();
+        System.out.println("Operation from super class: a decorated operation");
+    }
+}
