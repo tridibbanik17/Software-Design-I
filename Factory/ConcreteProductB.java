74989ade4996a6f9065a755cb86a85f1072928f8
Tridib Banik <tridib.perfect@gmail.com>
2025-04-28 02:49:58 -0400

    Add ConcreteProductB class implementing the Product interface

---
+++ Factory/ConcreteProductB.java
@@ -0,0 +1,6 @@
+public class ConcreteProductB implements Product {
+    @Override
+    public void performAction() {
+        System.out.println("ConcreteProductB is performing an action.");
+    }
+}
